a7304da37995a7cdbaf0241f62642d61597f7593
Yonas <ymul0001@student.monash.edu>
2020-01-19 23:35:30 +0700

    modify the hit button logic (1)

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let cards = [1,1,1,1,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,11,11,11,11,12,12,12,12,13,13,13,13]
 let totalCards = cards.length;
+let cardIndex = 0;
+let leftIncrement = 0.1;
+let cardLeftPosition = 20;
 const playerOneBoard = document.querySelector('.board__cards__one');
 const playerTwoBoard = document.querySelector('.board__cards__two');
 const deckCounter = document.querySelector('.deck__count span');
@@ -7,11 +10,12 @@ const hitButton = document.querySelector('.hit');
 const stayButton = document.querySelector('.stay');
 const foldButton = document.querySelector('.fold');
 
-const changeCardPosition = (className) => {
+
+const changeCardPosition = (className, leftPosition) => {
     let card = document.querySelector(className)
     card.style.opacity = '1';
     card.style.top = '35rem';
-    card.style.left = '20rem';
+    card.style.left = leftPosition.toString() + 'rem';
     card.style.visibility = 'visible';
 }
 
@@ -26,7 +30,51 @@ const createCard = (sourceUrl,alternateText) => {
 }
 
 const getCard = (number) => {
-    const card = createCard('./assets/images/2D.png','space-one')
+    let card;
+    switch(number) {
+        case 1:
+            card = createCard('./assets/images/AS.png','cards');
+            break;
+        case 2:
+            card = createCard('./assets/images/2D.png','cards');
+            break;
+        case 3:
+            card = createCard('./assets/images/3C.png','cards');
+            break;
+        case 4:
+            card = createCard('./assets/images/4H.png','cards');
+            break;
+        case 5:
+            card = createCard('./assets/images/5S.png','cards');
+            break;
+        case 6:
+            card = createCard('./assets/images/6D.png','cards');
+            break;
+        case 7:
+            card = createCard('./assets/images/7C.png','cards');
+            break;
+        case 8:
+            card = createCard('./assets/images/8S.png','cards');
+            break;
+        case 9:
+            card = createCard('./assets/images/9D.png','cards');
+            break;
+        case 10:
+            card = createCard('./assets/images/10H.png','cards');
+            break;
+        case 11:
+            card = createCard('./assets/images/JC.png','cards');
+            break;
+        case 12:
+            card = createCard('./assets/images/QD.png','cards');
+            break;
+        case 13:
+            card = createCard('./assets/images/KH.png','cards');
+            break;
+        default:
+            card = createCard('./assets/images/2D.png','cards');
+            break;
+    }
     playerTwoBoard.appendChild(card);
     console.log(playerTwoBoard);
 }
@@ -36,8 +84,22 @@ const updateDeckCounter = () => {
 }
 
 hitButton.addEventListener('click', () => {
-    changeCardPosition('.card img');
+    cardIndex = Math.floor(Math.random() * 52);
+    while (cards[cardIndex] === null)
+    {
+        cardIndex = Math.floor(Math.random() * 52);
+    }
+    getCard(cards[cardIndex]);
+    cards[cardIndex] = null; 
+    setTimeout(() => {
+        changeCardPosition('.card img', cardLeftPosition)
+        console.log(cardLeftPosition)
+    },10);
+    setTimeout(() => {
+        cardLeftPosition += leftIncrement;
+        console.log(cardLeftPosition);
+    },20)
+    console.log(cards);
 })
 
 updateDeckCounter();
-getCard(1);
